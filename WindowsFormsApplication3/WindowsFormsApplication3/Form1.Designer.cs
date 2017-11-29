d15440cef4630edc9dca65071763eebb7ef1a861
nguyenquoccuong18 <malengheo@gmail.com>
2017-11-29 23:42:18 +0700

    control tower
    control tower

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.ptb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Image = global::WindowsFormsApplication3.Properties.Resources.Untitled4;
+            this.button4.Location = new System.Drawing.Point(491, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 99);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::WindowsFormsApplication3.Properties.Resources.Untitled5;
+            this.button3.Location = new System.Drawing.Point(331, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // bt2
+            // 
+            this.bt2.Image = global::WindowsFormsApplication3.Properties.Resources.Untitled6;
+            this.bt2.Location = new System.Drawing.Point(32, 73);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(86, 81);
+            this.bt2.TabIndex = 3;
+            this.bt2.Text = "bt2";
+            this.bt2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WindowsFormsApplication3.Properties.Resources.Untitled1;
+            this.button1.Location = new System.Drawing.Point(314, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 213);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // bt1
+            // 
+            this.bt1.Image = global::WindowsFormsApplication3.Properties.Resources.maybay_jpg;
+            this.bt1.Location = new System.Drawing.Point(32, 160);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(86, 81);
+            this.bt1.TabIndex = 1;
+            this.bt1.Text = "bt1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ptb1
+            // 
+            this.ptb1.Image = global::WindowsFormsApplication3.Properties.Resources.Untitled3;
+            this.ptb1.Location = new System.Drawing.Point(12, 12);
+            this.ptb1.Name = "ptb1";
+            this.ptb1.Size = new System.Drawing.Size(943, 539);
+            this.ptb1.TabIndex = 0;
+            this.ptb1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 549);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.ptb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptb1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
